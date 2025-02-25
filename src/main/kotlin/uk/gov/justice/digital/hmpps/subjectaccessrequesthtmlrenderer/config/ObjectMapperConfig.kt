0c64ae1e30eba6e3c736641062746eac3878676b
Dave Llewellyn <david.llewellyn@digital.justice.gov.uk>
2025-02-25 16:06:22 +0000

    fix auth client secrets

--- src/main/kotlin/uk/gov/justice/digital/hmpps/subjectaccessrequesthtmlrenderer/config/ObjectMapperConfig.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/subjectaccessrequesthtmlrenderer/config/ObjectMapperConfig.kt
@@ -4,10 +4,13 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import org.springframework.context.annotation.Bean
 import org.springframework.stereotype.Component
+import java.text.SimpleDateFormat
 
 @Component
 class ObjectMapperConfig {
 
   @Bean
-  fun objectMapper(): ObjectMapper = ObjectMapper().registerModule(JavaTimeModule())
+  fun objectMapper(): ObjectMapper = ObjectMapper()
+    .registerModule(JavaTimeModule())
+    .setDateFormat(SimpleDateFormat("yyyy-MM-dd"))
 }
