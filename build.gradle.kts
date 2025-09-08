814de141d0a03f2655a2f11112d10477dc91f430
Simon Mitchell <51327941+simon-mitchell@users.noreply.github.com>
2025-09-08 14:14:14 +0100

    HAAR-4272: ⬆️ update to spring-boot-plugin-9.0.1 (#80)
    * HAAR-4272: ⬆️ update to spring-boot-plugin-9.0.1
    
    * HAAR-4272: update github action to use central workflow for gradle varify
    
    * HAAR-4272: revert aws.sdk.kotlin to s3:1.4.125
    
    * Empty Commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "9.0.0"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "9.0.1"
   kotlin("plugin.spring") version "2.2.10"
 }
 
@@ -10,8 +10,8 @@ configurations {
 dependencies {
   implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.5.0")
   implementation("org.springframework.boot:spring-boot-starter-webflux")
-  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.11")
-  implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.19.2")
+  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.13")
+  implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.20.0")
   implementation("org.springframework.boot:spring-boot-starter-mustache")
   implementation("com.github.spullara.mustache.java:compiler:0.9.14")
   implementation("com.github.jknack:handlebars:4.5.0")
@@ -19,7 +19,7 @@ dependencies {
   implementation("aws.sdk.kotlin:s3:1.4.125")
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.10.2")
-  implementation("io.opentelemetry:opentelemetry-extension-kotlin:1.52.0")
+  implementation("io.opentelemetry:opentelemetry-extension-kotlin:1.54.0")
 
   runtimeOnly("org.flywaydb:flyway-core")
   runtimeOnly("com.h2database:h2:2.3.232")
@@ -28,7 +28,7 @@ dependencies {
 
   testImplementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter-test:1.5.0")
   testImplementation("org.wiremock:wiremock-standalone:3.13.1")
-  testImplementation("io.swagger.parser.v3:swagger-parser:2.1.32") {
+  testImplementation("io.swagger.parser.v3:swagger-parser:2.1.33") {
     exclude(group = "io.swagger.core.v3")
   }
 }
