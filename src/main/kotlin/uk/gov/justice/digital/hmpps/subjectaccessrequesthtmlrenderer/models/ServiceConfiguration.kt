0575e1490e2f931e2cdfc8b54c067c44edd1cd53
DaveLlewellynMoJ <165893357+DaveLlewellynMoJ@users.noreply.github.com>
2025-11-04 12:15:25 +0000

    added template version and service configuration models and repos (#95)

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/subjectaccessrequesthtmlrenderer/models/ServiceConfiguration.kt
@@ -0,0 +1,43 @@
+package uk.gov.justice.digital.hmpps.subjectaccessrequesthtmlrenderer.models
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+import java.util.UUID
+
+@Entity
+@Table(name = "SERVICE_CONFIGURATION")
+data class ServiceConfiguration(
+
+  @Id
+  var id: UUID = UUID.randomUUID(),
+
+  @Column(name = "service_name", nullable = false)
+  var serviceName: String,
+
+  @Column(name = "label", nullable = false)
+  var label: String,
+
+  @Column(name = "url", nullable = false)
+  var url: String,
+
+  @Column(name = "list_order", nullable = false)
+  var order: Int,
+
+  @Column(name = "enabled", nullable = false)
+  var enabled: Boolean,
+
+  @Column(name = "template_migrated", nullable = false)
+  var templateMigrated: Boolean,
+) {
+  constructor() : this(
+    id = UUID.randomUUID(),
+    serviceName = "",
+    label = "",
+    url = "",
+    order = 0,
+    enabled = false,
+    templateMigrated = false,
+  )
+}
