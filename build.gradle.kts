2e70e97d9a512789f221ad0a954e285f26fdb64a
simon.mitchell <simon.mitchell1@justice.gov.uk>
2026-05-13 20:06:29 +0100

    HAAR-5383: ⬆️ update to spring-boot-plugin-10.3.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
 
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "10.2.5"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "10.3.0"
 
   kotlin("plugin.spring") version "2.3.21"
 }
